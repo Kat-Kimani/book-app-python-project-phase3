7a3802ae44223f245e1eaeaa4ef294bd06ce1edc
catherine <wachukimani@gmail.com>
2023-06-07 21:41:02 +0300

    created  books class

---
+++ migration/versions/38739e7fced3_first_init.py
@@ -0,0 +1,24 @@
+"""first init
+
+Revision ID: 38739e7fced3
+Revises: 
+Create Date: 2023-06-07 19:52:33.854803
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38739e7fced3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
